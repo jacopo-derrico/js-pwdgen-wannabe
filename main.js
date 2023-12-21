61d451bdf4a3159a8f28668dbd40b1d990e71e38
jacopo-derrico <derricoj.design@gmail.com>
2023-12-21 14:14:13 +0100

    variables and console/body output

---
+++ main.js
@@ -0,0 +1,8 @@
+//variabili
+let nomeUtente = prompt('Inserisci nome');
+let cognomeUtente = prompt('Inserisci cognome');
+let colorePreferito = prompt('Inserisci colore preferito');
+
+
+console.log(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito);
+document.writeln(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito);
