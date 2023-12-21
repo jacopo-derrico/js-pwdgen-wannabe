a7902c3a0c20addb3d4f4d6fcebb81484b17ece4
jacopo-derrico <derricoj.design@gmail.com>
2023-12-21 15:15:24 +0100

    added visual and fixed psswgenerator

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ let cognomeUtente = prompt('Inserisci cognome');
 let colorePreferito = prompt('Inserisci colore preferito');
 
 
-console.log(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito);
-document.writeln(nomeUtente + ' ' + cognomeUtente + ' ' + colorePreferito);
+console.log(nomeUtente + cognomeUtente + colorePreferito);
+document.writeln('<p class="password">' + nomeUtente, cognomeUtente, colorePreferito + '</p>');
+
